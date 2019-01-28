9ab8b95cc8f36f3f80f4e7ac686600bae96f2129
Szilagyi Illes <szi.illes85@gmail.com>
2019-01-28 11:13:10 +0200

    initialCommit

---
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.memorygame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+   /* private ImageView
+            x1y1, x2y1, x3y1,
+            x1y2, x2y2, x3y2,
+            x1y3, x2y3, x3y3,
+            x1y4, x2y4, x3y4;*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       /* ImageView x1y1 = (ImageView) findViewById(R.id.x1y1);
+        ImageView x2y2 = (ImageView) findViewById(R.id.x2y2);
+        ImageView x3y3 = (ImageView) findViewById(R.id.x3y3);
+        ImageView x1y4 = (ImageView) findViewById(R.id.x1y4);
+        ImageView x2y1 = (ImageView) findViewById(R.id.x2y1);
+        ImageView x3y2 = (ImageView) findViewById(R.id.x3y2);
+        ImageView x1y3 = (ImageView) findViewById(R.id.x1y3);
+        ImageView x2y4 = (ImageView) findViewById(R.id.x2y4);
+        ImageView x3y1 = (ImageView) findViewById(R.id.x3y1);
+        ImageView x1y2 = (ImageView) findViewById(R.id.x1y2);
+        ImageView x2y3 = (ImageView) findViewById(R.id.x2y3);
+        ImageView x3y4 = (ImageView) findViewById(R.id.x3y4);
+
+        x1y1.setImageResource(R.drawable.card_reverse_side);
+
+        x1y1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
+
+
+    }
+}
